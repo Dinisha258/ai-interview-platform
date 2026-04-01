2d49d7d22932c7ea446759785f92a21e09bd359e
JesCaim <1920578206@qq.com>
2026-04-01 13:39:13 +0800

    refactor：controller目录重构

--- aip-backend/ruoyi-system/src/main/java/com/ruoyi/aip/controller/manage/AipKnowledgeDocController.java
+++ aip-backend/ruoyi-system/src/main/java/com/ruoyi/aip/controller/manage/AipKnowledgeDocController.java
@@ -1,8 +1,8 @@
-package com.ruoyi.aip.controller;
+package com.ruoyi.aip.controller.manage;
 
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
